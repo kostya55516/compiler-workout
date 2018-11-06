175a02e94eddb2832bb6b93d3245ec0ab35ff97a
Konstantin Nedikov <konstantin.a.nedikov@gmail.com>
2018-11-06 16:32:20 +0300

    complete mthf

--- src/Language.ml
+++ src/Language.ml
@@ -63,7 +63,7 @@ module Expr =
                                                             
     (* Expression evaluator
 
-          val eval : env -> config -> t -> config
+          val eval : env -> config -> t -> int * config
 
 
        Takes an environment, a configuration and an expresion, and returns another configuration. The 
@@ -72,17 +72,91 @@ module Expr =
            method definition : env -> string -> int list -> config -> config
 
        which takes an environment (of the same type), a name of the function, a list of actual parameters and a configuration, 
-       an returns resulting configuration
-    *)                                                       
-    let rec eval env ((st, i, o, r) as conf) expr = failwith "Not implemented"
-         
+       an returns a pair: the return value for the call and the resulting configuration
+    *)                                              
+        
+    let to_func op =
+      let bti   = function true -> 1 | _ -> 0 in
+      let itb b = b <> 0 in
+      let (|>) f g   = fun x y -> f (g x y) in
+      match op with
+      | "+"  -> (+)
+      | "-"  -> (-)
+      | "*"  -> ( * )
+      | "/"  -> (/)
+      | "%"  -> (mod)
+      | "<"  -> bti |> (< )
+      | "<=" -> bti |> (<=)
+      | ">"  -> bti |> (> )
+      | ">=" -> bti |> (>=)
+      | "==" -> bti |> (= )
+      | "!=" -> bti |> (<>)
+      | "&&" -> fun x y -> bti (itb x && itb y)
+      | "!!" -> fun x y -> bti (itb x || itb y)
+      | _    -> failwith (Printf.sprintf "Unknown binary operator %s" op)    
+    
+    let fill_and_enter st params locals args =
+          let st                    = State.enter st (params @ locals) in
+          let update st (p, a)      = State.update p a st in
+          List.fold_left update st (List.combine params args)
+
+
+    let rec eval env ((st, i, o, r) as conf) expr =      
+      match expr with
+      | Const n -> n, (st, i, o, Some n)
+      | Var   x -> let n = State.eval st x in n, (st, i, o, Some n)
+      | Binop (op, x, y) -> 
+        let a, conf = eval env conf x in
+        let b, conf = eval env conf y in
+        let n       = to_func op a b in
+        n, (st, i, o, Some n)
+      | Call (name, exps) ->
+          let eval_list exp (ags, c) = let a, c = eval env c exp in (a::ags, c) in
+          let args, conf             = List.fold_right eval_list exps ([], conf) in
+          let st', i', o', Some r'   = env#definition env name args conf in
+          r', (State.leave st' st, i', o', Some r')
+
     (* Expression parser. You can use the following terminals:
 
          IDENT   --- a non-empty identifier a-zA-Z[a-zA-Z0-9_]* as a string
          DECIMAL --- a decimal constant [0-9]+ as a string                                                                                                                  
     *)
-    ostap (                                      
-      parse: empty {failwith "Not implemented"}
+
+    let binop op x y = Binop (op, x, y)
+
+    ostap (
+      expr:
+        !(Util.expr
+           (fun x -> x)
+           [|
+             `Lefta , [ostap ("!!"), (binop "!!")];
+             `Lefta , [ostap ("&&"), (binop "&&")];
+             `Nona  , [ostap ("<="), (binop "<=");
+                       ostap (">="), (binop ">=");
+                       ostap ("<"), (binop "<");
+                       ostap (">"), (binop ">")];
+             `Nona  , [ostap ("=="), (binop "==");
+                       ostap ("!="), (binop "!=")];
+             `Lefta , [ostap ("+"), (binop "+");
+                       ostap ("-"), (binop "-")];
+             `Lefta , [ostap ("*"), (binop "*");
+                       ostap ("/"), (binop "/");
+                       ostap ("%"), (binop "%")]
+           |]
+           primary
+         );
+      
+      func: fname:IDENT "(" a:args ")" { Call (fname, a) };
+
+      args:
+        e:expr "," a:args   { e :: a }
+      | e:expr              { [e] }
+      | empty               { [] }
+      ;
+
+      primary: func | x:IDENT {Var x} | n:DECIMAL {Const n}  | -"(" expr -")";
+
+      parse: expr
     )
     
   end
@@ -106,16 +180,87 @@ module Stmt =
                                                                     
     (* Statement evaluator
 
-         val eval : env -> config -> t -> config
+         val eval : env -> config -> t -> t -> config
 
        Takes an environment, a configuration and a statement, and returns another configuration. The 
        environment is the same as for expressions
     *)
-    let rec eval env ((st, i, o, r) as conf) k stmt = failwith "Not implemnted"
+
+    
+    let rec eval env ((st, i, o, r) as conf) k stmt =
+      let (<>) s1 s2 = if s2 == Skip then s1 else Seq (s1, s2) in
+      match stmt with
+      | Skip -> if k == Skip then conf else eval env conf Skip k
+      | Assign (s, exp) -> 
+        let n, (st, i, o, r) = Expr.eval env conf exp in
+        let st               = State.update s n st in
+        eval env (st, i, o, r) Skip k
+      | Write exp -> 
+        let n, (st, i, o, r) = Expr.eval env conf exp in
+        eval env (st, i, o @ [n], r) Skip k
+      | Read s -> (
+        match i with
+        | n::i -> eval env ((State.update s n st), i, o, r) Skip k
+        | _ -> failwith "Nothig to read"
+      )
+      | Seq (s1, s2) -> eval env conf (s2 <> k) s1    
+      | If (exp, s1, s2) ->
+        let n, conf = Expr.eval env conf exp in
+        let s       = if n != 0 then s1 else s2 in
+        eval env conf k s
+      | While (exp, s) -> 
+        let n, conf = Expr.eval env conf exp in
+        let k, s    = if n != 0 then While (exp, s) <> k, s else Skip, k in 
+        eval env conf k s
+      | Repeat (s, exp) -> 
+        (* let conf    = eval env conf Skip s in
+        let n, conf = Expr.eval env conf exp in 
+        let k, s    = if n == 0 then k, Repeat (s, exp) else Skip, k in
+        eval env conf k s *)
+        let exp = Expr.Binop ("==", exp, Const 0) in
+        eval env conf (While (exp, s) <> k) s
+      | Call (name, exps) -> 
+        let conf = snd ( Expr.eval env conf (Expr.Call (name, exps)) ) in
+        eval env conf Skip k
+      | Return r ->
+        match r with
+        | None   -> conf
+        | Some e -> snd (Expr.eval env conf e)
+
          
     (* Statement parser *)
     ostap (
-      parse: empty {failwith "Not implemented"}
+      expr: !(Expr.expr);
+
+      stmt:
+        x:IDENT ":=" e:expr     { Assign (x, e) }
+      | "read" "(" x:IDENT ")"  { Read x }
+      | "write" "(" e:expr ")"  { Write e }
+      | "skip"                  { Skip }
+      | "if" e:expr "then" s1:parse "elif" s2:elif "fi"           { If (e, s1, s2) }
+      | "if" e:expr "then" s1:parse "else" s2:parse "fi"          { If (e, s1, s2) }
+      | "if" e:expr "then" s1:parse "fi"                          { If (e, s1, Skip) }
+      | "while" e:expr "do" s:parse "od"                          { While (e, s) }
+      | "for"   s1:stmt "," e:expr "," s2:stmt "do" s:parse "od"  { Seq (s1, While(e, Seq(s, s2))) }
+      | "repeat" s:parse "until" e:expr                           { Repeat (s, e) }
+      | fname:IDENT "(" a:args ")"                                { Call (fname, a) }
+      | "return" e:expr                                           { Return (Some e) }
+      | "return"                                                  { Return None }
+      ;
+
+      args:
+        e:expr "," a:args   { e :: a }
+      | e:expr              { [e] }
+      | empty               { [] }
+      ;
+ 
+      elif:
+        e:expr "then" s1:parse "elif" s2:elif             { If (e, s1, s2) }
+      | e:expr "then" s1:parse "else" s2:parse            { If (e, s1, s2) }
+      | e:expr "then" s1:parse                            { If (e, s1, Skip) }
+      ;
+
+      parse: <x::xs> :!(Util.listBy)[ostap (";")][stmt] { List.fold_left (fun x y -> Seq (x, y)) x xs }
     )
       
   end
@@ -127,8 +272,13 @@ module Definition =
     (* The type for a definition: name, argument list, local variables, body *)
     type t = string * (string list * string list * Stmt.t)
 
-    ostap (     
-      parse: empty {failwith "Not implemented"}
+    ostap (
+      arg  : IDENT;
+      parse: %"fun" name:IDENT "(" args:!(Util.list0 arg) ")"
+         locs:(%"local" !(Util.list arg))?
+        "{" body:!(Stmt.parse) "}" {
+        (name, (args, (match locs with None -> [] | Some l -> l), body))
+      }
     )
 
   end
@@ -150,8 +300,8 @@ let eval (defs, body) i =
   let _, _, o, _ =
     Stmt.eval
       (object
-         method definition env f args (st, i, o, r) =                                                                      
-           let xs, locs, s      =  snd @@ M.find f m in
+         method definition env f args (st, i, o, r) =
+           let xs, locs, s      = snd @@ M.find f m in
            let st'              = List.fold_left (fun st (x, a) -> State.update x a st) (State.enter st (xs @ locs)) (List.combine xs args) in
            let st'', i', o', r' = Stmt.eval env (st', i, o, r) Stmt.Skip s in
            (State.leave st'' st, i', o', r')
--- src/X86.ml
+++ src/X86.ml
@@ -90,14 +90,170 @@ open SM
    Take an environment, a stack machine program, and returns a pair --- the updated environment and the list
    of x86 instructions
 *)
-let compile env code = failwith "Not implemented"
+let suffix op = match op with
+    | "<" -> "l"
+    | "<=" -> "le"
+    | ">" -> "g"
+    | ">=" -> "ge"
+    | "==" -> "e"
+    | "!=" -> "ne"
+
+let rec compile env code =
+  let onstack = function | S _ -> true | _ -> false in
+  match code with
+  | [] -> env, []
+  | inst :: code' ->
+    let env, xcode =
+      match inst with
+        | LD x    -> let s, env = (env#global x)#allocate in
+                      env, (match s with
+                            | M _ | S _ -> [Mov (env#loc x, eax); Mov (eax, s)]
+                            | _        -> [Mov (env#loc x, s)]
+                           )
+        | ST x    -> let s, env = (env#global x)#pop in
+                      env, [Mov (s, env#loc x)]
+        | READ    -> let s, env = env#allocate in
+                      env, [Call "Lread"; Mov (eax, s)]
+        | WRITE   -> let s, env = env#pop in
+                      env, [Push s; Call "Lwrite"; Pop eax]
+        | CONST z -> let s, env = env#allocate in
+                      env, [Mov (L z, s)]
+
+        | LABEL l     -> env, [Label l]
+        | JMP l       -> env, [Jmp l]
+        | CJMP (t, l) -> let s, env = env#pop in
+                          env, [Binop ("cmp", L 0, s); CJmp (t, l)]
+
+        | BINOP op  ->
+        let x, y, env = env#pop2 in
+          env#push y, 
+          (match op with
+          | "*"  -> 
+                  if onstack x && onstack y
+                  then [Mov (y, eax); Binop(op, x, eax); Mov(eax, y)]
+                  else [Binop (op, x, y)] 
+          | "+" | "-" -> 
+                  if onstack x && onstack y
+                  then [Mov (x, eax); Binop(op, eax, y)]
+                  else [Binop (op, x, y)] 
+          | "%" | "/" -> 
+                  [Mov (y, eax);
+                  Cltd;
+                  IDiv x;
+                  Mov ((match op with
+                    | "/" -> eax
+                    | _ -> edx), y)
+                  ]
+          | "<=" | "<" | ">=" | ">" | "==" | "!=" ->
+            [
+              Binop ("^", eax, eax);
+              Mov   (x, edx);
+              Binop ("cmp", edx, y);
+              Set   (suffix op, "%al");
+              Mov   (eax, y)
+            ]
+          | "!!" -> 
+            [
+              Mov   (y, eax);
+              Binop (op, x, eax);
+              Mov   (L 0, eax);
+              Set   ("ne", "%al");
+              Mov   (eax, y)
+            ]
+          | "&&" -> 
+            [
+              Mov   (L 0, eax);
+              Binop ("cmp", eax, x);
+              Set   ("ne", "%al");
+              Mov   (L 0, edx);
+              Binop ("cmp", edx, y);
+              Set   ("ne", "%dl");
+              Binop (op, eax, edx);
+              Mov   (edx, y)
+            ]
+          )
+        | CALL (f, n, b)  ->  
+          let rec gen n f e =
+            match n with
+            | 0 -> [], e  
+            | _ ->  let x, e  = f e in
+                    let xs, e = gen (n - 1) f e in
+                    x::xs, e
+          in
+          let args, env = gen n (fun env -> env#pop) env in
+          let live      = env#live_registers in
+          let start     = 
+            match fst (env#allocate) with
+            | S k -> k
+            | _   -> 0
+          in 
+          let end_p     = start + List.length live + n - 1 in
+          let args_push = 
+            let args_push = List.mapi 
+              (fun i -> let s_to = M (Printf.sprintf "-%d(%%esp)" ((end_p - i + 1) * word_size)) in function 
+                          | R a -> [Mov (R a, s_to)]
+                          | a   -> [Mov (a, eax); Mov (eax, s_to)]
+                )
+              args
+            in List.flatten args_push
+          in 
+          let save_live   = List.fold_right (fun a xs -> (Push a)::xs) live [] in
+          let load_live   = List.fold_left (fun xs a -> (Pop a)::xs) [] live in
+          let ret, env    = if b then let s, env = env#allocate in [Mov (eax, s)], env else [], env in
+          env, [Meta ("#CALL " ^ f)] 
+              @ args_push
+              @ [Binop ("-", L (start * word_size), esp)] 
+              @ save_live 
+              @ [Binop ("-", L (n * word_size), esp);
+                 Call f;
+                 Binop ("+", L (n * word_size), esp)] 
+              @ load_live 
+              @ ret
+
+
+          (* let rec gen n f e =
+            match n with
+            | 0 -> [], e  
+            | _ ->  let x, e  = f e in
+                    let xs, e = gen (n - 1) f e in
+                    (Push x)::xs, e
+          in
+          let push_a, env = gen n (fun env -> env#pop) env in
+          let live        = env#live_registers in
+          let save_live   = List.fold_right (fun a xs -> (Push a)::xs) live [] in
+          let load_live   = List.fold_left (fun xs a -> (Pop a)::xs) [] live in
+          let ret, env    = if b then let s, env = env#allocate in [Mov (eax, s)], env else [], env in
+          env, save_live @ push_a @ [Call f; Binop ("+", L (n * word_size), esp)] @ ret @ load_live
+ *)
+        | BEGIN (f, a, l) ->  let env = env#enter f a l in
+          env, [Push ebp; Mov (esp, ebp); Binop ("-", M ("$" ^ env#lsize), esp)]
+
+          (* создать метку; создать переменные; заполнить переменные из стека; пролог*)
+        | END             ->  env, [Label (env#epilogue); Mov (ebp, esp); Pop ebp; Ret;
+                                    Meta (Printf.sprintf "\t.set\t%s,\t%d" env#lsize (env#allocated * word_size))]
+
+        (* метка эпилога; сдлеать эпилог*)
+        | RET b           ->  
+          let env, ret =
+            match b with
+            | false -> env, []
+            | true  -> let s, env = env#pop in env, [Mov (s, eax)] 
+          in
+          env, ret @ [Jmp (env#epilogue)] (* положить или нет значение на стек; перейти на эпилог *)
+    in
+    let env, xcode' = compile env code' in
+      env, xcode @ xcode'
                                 
 (* A set of strings *)           
 module S = Set.Make (String)
 
+let list_init n f =
+  let rec list_init n f i = if i < n then (f i)::(list_init n f (i + 1)) else [] in
+  list_init n f 0
+
 (* Environment implementation *)
-let make_assoc l = List.combine l (List.init (List.length l) (fun x -> x))
-                     
+let make_assoc l = List.combine l (list_init (List.length l) (fun x -> x))
+
 class env =
   object (self)
     val globals     = S.empty (* a set of global variables         *)
@@ -116,14 +272,14 @@ class env =
     (* allocates a fresh position on a symbolic stack *)
     method allocate =    
       let x, n =
-	let rec allocate' = function
-	| []                            -> ebx     , 0
-	| (S n)::_                      -> S (n+1) , n+1
-	| (R n)::_ when n < num_of_regs -> R (n+1) , stack_slots
-        | (M _)::s                      -> allocate' s
-	| _                             -> S 0     , 1
-	in
-	allocate' stack
+    	let rec allocate' = function
+    	| []                            -> R 0     , 0
+    	| (S n)::_                      -> S (n+1) , n+1
+    	| (R n)::_ when n < num_of_regs -> R (n+1) , stack_slots
+      | (M _)::s                      -> allocate' s
+    	| _                             -> S 0     , 1
+    	in
+    	allocate' stack
       in
       x, {< stack_slots = max n stack_slots; stack = x::stack >}
 
